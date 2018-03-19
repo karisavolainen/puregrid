ecd22daa6047829b84ba669f0b574b1cf6847251
Kari Savolainen <kari@seravo.com>
2018-03-19 16:14:10 +0200

    remove emoji mess

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 
 /* Create Proper WordPress Titles */
 add_theme_support( 'title-tag' );
+/* Support featured images */
+add_theme_support( 'post-thumbnails' );
 
 /* Main navigation */
 function register_my_menu() {
@@ -32,4 +34,21 @@ if( function_exists('acf_add_options_page') ) {
                         'capability'    => 'publish_pages',
                         'redirect'              => false
                 ));
+}
+
+/* Disable Emoji Mess */
+function disable_wp_emojicons() {
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+	add_filter( 'emoji_svg_url', '__return_false' );
+}
+add_action( 'init', 'disable_wp_emojicons' );
+function disable_emojicons_tinymce( $plugins ) {
+	return is_array( $plugins ) ? array_diff( $plugins, array( 'wpemoji' ) ) : array();
 }
